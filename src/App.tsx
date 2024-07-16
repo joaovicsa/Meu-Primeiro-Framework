422ffaaf3586af1cc167857814ecb76db5714f9b
João Sá <joao.vsaa90@gmail.com>
2024-07-16 11:05:37 -0300

    add: init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import LivroLista from './LivroLista';
+
+function App() {
+  return (
+    <div className="App">
+          <LivroLista />
+    </div>
+  );
+}
+
+export default App;
