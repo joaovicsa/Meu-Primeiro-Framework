2125ae29cd777aa066054f575ef23dd644a3ef69
João Sá <joao.vsaa90@gmail.com>
2024-08-06 11:57:26 -0300

    styled: adiciona estilização com o BootStrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,36 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import LivroLista from './LivroLista';
 import LivroDados from './LivroDados';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
-        <Link className='navbar-brand' to='/'>Catálogo</Link>
-        <Link className='navbar-brand' to='/dados'>Novo</Link>
-      </nav>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Catálogo</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/dados">Novo</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </nav>
       <Routes>
         <Route path='/' element={<LivroLista />} />
         <Route path='/dados' element={<LivroDados />} />
