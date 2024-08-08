755c405ded9677763cb0e06634cb25e638a5703e
João Sá <joao.vsaa90@gmail.com>
2024-08-08 20:40:32 -0300

    add: pagina inicial

--- livros-next/componentes/Menu.tsx
+++ livros-next/componentes/Menu.tsx
@@ -5,18 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Menu: React.FC = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Link href="/" passHref>
-        <Navbar.Brand>Minha Aplicação</Navbar.Brand>
       </Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Link href="/" passHref>
-            <Nav.Link as="span">Início</Nav.Link>
+            <Nav.Link as="span">Home</Nav.Link>
           </Link>
           <Link href="/LivroLista" passHref>
-            <Nav.Link as="span">Lista de Livros</Nav.Link>
+            <Nav.Link as="span">Catálogo</Nav.Link>
           </Link>
           <Link href="/LivroDados" passHref>
             <Nav.Link as="span">Dados do Livro</Nav.Link>
--- livros-next/pages/index.tsx
+++ livros-next/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Menu from '../componentes/Menu';
-import styles from '../styles/Home.module.css'; 
+import styles from '../styles/Home.module.css';
 
 const Home: React.FC = () => {
   return (
